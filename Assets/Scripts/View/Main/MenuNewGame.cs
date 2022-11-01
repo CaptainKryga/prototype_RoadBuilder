be01e8f6a0a533122171468709fd81a7f549ff51
CaptainKryga <krygadenis@gmail.com>
2022-11-01 11:56:12 +0200

    add main menu script's logic

---
+++ Assets/Scripts/View/Main/MenuNewGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace View.Main
+{
+    public class MenuNewGame : MenuMainBase
+    {
+        public void OnClick_Random()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
